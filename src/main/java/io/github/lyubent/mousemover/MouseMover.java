58f226cb28cc14f5b48225ea84d7051fc454fc82
lyubent <lyuben@mail.com>
2023-02-11 16:25:14 +0000

    Revert to using Thread#sleep for performance

--- src/main/java/io/github/lyubent/mousemover/MouseMover.java
+++ src/main/java/io/github/lyubent/mousemover/MouseMover.java
@@ -21,27 +21,20 @@ public class MouseMover {
         try {
             Platform.startup(() -> robot = new Robot());
             Instant start = Instant.now();
-            Instant lastUpdate = Instant.now();
             // workaround - enables usage of var inside lambda as value is never reassigned - thus its effectively final
             AtomicInteger iterations = new AtomicInteger(0);
 
             // run for two hours
             while (Duration.between(start, Instant.now()).compareTo(Duration.of(2, ChronoUnit.HOURS)) < 0) {
-                // have five seconds passed - avoids usage of Thread.sleep
-                if (Duration.between(lastUpdate, Instant.now()).compareTo(Duration.of(5, ChronoUnit.SECONDS)) < 0) {
-                    logger.trace("Skipping update, not enough time has passed.");
-                    continue;
-                }
-
                 logger.trace("Updating mouse position");
-                lastUpdate = Instant.now();
-
                 Platform.runLater(() -> {
                     double newMouseX = iterations.get() % 2 == 0 ? robot.getMouseX() + MOVEMENT_SPAN
                                                                  : robot.getMouseX() - MOVEMENT_SPAN;
                     robot.mouseMove(new Point2D(newMouseX, robot.getMouseY()));
                 });
                 iterations.getAndIncrement();
+                // todo - this cannot be on the main thread, use an executor
+                Thread.sleep(Duration.of(5, ChronoUnit.SECONDS));
             }
         } catch (Exception ex) {
             logger.error("Caught unrecoverable exception. Terminating application", ex);
