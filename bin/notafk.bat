f6b6bf05cad93e7c00ba6cfd2d2d0926c05a93ca
lyubent <lyuben@mail.com>
2023-02-12 23:17:09 +0000

    Add basic batch and sh files to launch the jar

---
+++ bin/notafk.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+REM assumes jar location defaults to target build dir
+start java -jar ..\target\notafk-1.0.1-shaded.jar
---
+++ bin/notafk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# assumes jar location defaults to target build dir
+java -jar ../target/notafk-1.0.1-shaded.jar
